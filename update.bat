1153d8f1fb4d1881ddc1c383f552ea7ffff0c574
Toke Jepsen <tokejepsen@gmail.com>
2016-05-14 12:39:40 +0100

    conda working version

--- update.bat
+++ update.bat
@@ -1,3 +1,16 @@
 cd %~dp0
-start /W %~dp0vendors\git\bin\git.exe pull
-%~dp0vendors\Python27\python.exe %~dp0repositories.py
+
+REM downloading the miniconda install executable seems very slow
+REM SET "FILENAME=%~dp0miniconda.exe"
+REM bitsadmin.exe /transfer "Miniconda Download" https://repo.continuum.io/miniconda/Miniconda2-latest-Windows-x86_64.exe "%FILENAME%"
+
+IF EXIST %~dp0miniconda GOTO INSTALLEXISTS
+%~dp0miniconda.exe /RegisterPython=0 /AddToPath=0 /S /D=%~dp0miniconda
+:INSTALLEXISTS
+
+set PATH=%~dp0miniconda;%~dp0miniconda\Scripts
+
+conda install git -y
+start /W %~dp0miniconda\Library\bin\git.exe pull
+
+conda env create -f %~dp0environment.yml
