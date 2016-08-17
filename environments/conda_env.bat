86bc9d1051da46a01ac1eab831f46a4ebc59d3bf
Toke Jepsen <tokejepsen@gmail.com>
2016-08-17 16:21:49 +0100

    downloading latest miniconda executable instead of including in repo

--- environments/conda_env.bat
+++ environments/conda_env.bat
@@ -1,4 +1,7 @@
 IF EXIST %~dp0..\miniconda GOTO INSTALLEXISTS
+SET "FILENAME=%~dp0..\miniconda.exe"
+SET "URL=https://repo.continuum.io/miniconda/Miniconda2-latest-Windows-x86_64.exe"
+powershell "Import-Module BitsTransfer; Start-BitsTransfer '%URL%' '%FILENAME%'"
 %~dp0..\miniconda.exe /RegisterPython=0 /AddToPath=0 /S /D=%~dp0..\miniconda
 :INSTALLEXISTS
 
